8d09f3c8d81e37d9eeb0af3d3f49d52d3a7a9fd0
Aibar Berekeyev <aibar.berekeyev@gmail.com>
2024-04-14 01:52:10 +0500

    CORS Added

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,8 +9,18 @@ from .dependencies import get_password_hash, create_access_token, get_current_us
 from .database import get_db
 from .routers import student_router, register_router, test_details_router
 from .routers import student_router, register_router, teacher_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(student_router.router)
 app.include_router(register_router.router)
